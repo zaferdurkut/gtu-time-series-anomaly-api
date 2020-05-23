faf62f35d1d63664ccbf5ac01e3f795f90018187
Zafer Durkut <durkutzafer@gmail.com>
2020-05-24 00:57:13 +0300

    project base was updated-2

--- src/api/common_controller.py
+++ src/api/common_controller.py
@@ -3,8 +3,6 @@ from fastapi import APIRouter
 
 from src.api.dto.heartbeat_output_dto import HeartbeatOutputModel
 from src.api.dto.service_check_output_dto import ServiceCheckOutputModel
-from src.infra.bigquery_adapter.bigquery_adapter import BigQueryAdapter
-from src.infra.postgres_adapter.postgres_adapter import PostgresAdapter
 from src.infra.redis_adapter.redis_adapter import RedisAdapter
 
 router = APIRouter()
@@ -18,7 +16,5 @@ def heartbeat():
 @router.get("/service-check", response_model=ServiceCheckOutputModel, status_code=200)
 def service_check():
     return ServiceCheckOutputModel.to_model(
-        db_connection_check=PostgresAdapter.service_check(),
-        big_query_check=BigQueryAdapter.service_check(),
         redis_cache_check=RedisAdapter.service_check()
     )
