6b6ff5236ad8c226ff2845d7c8d65687a44271cb
Zafer Durkut <durkutzafer@gmail.com>
2020-05-26 12:19:05 +0300

    JPLService controller  was created

--- src/api/jpl_controller.py
+++ src/api/jpl_controller.py
@@ -8,7 +8,9 @@ from starlette.responses import StreamingResponse, FileResponse
 
 from src.api.dto.jpl_all_list_output_dto import JPLAllListOutputModel
 from src.api.dto.jpl_data_output_dto import JPLDataOutputModel
+from src.core.service.jpl_service import JPLService
 from src.infra.client.jpl.jpl import JPLClient
+from src.core.model.anomaly_dto import GeneralAnomalyTotalModel
 
 router = APIRouter()
 
@@ -23,6 +25,7 @@ def get_all_items():
     result = JPLClient.get_all_list()
     return JPLAllListOutputModel(items=result)
 
+
 @router.get("/base-data", response_model=JPLDataOutputModel, status_code=200)
 def get_base_data(data_name):
     """
@@ -36,3 +39,25 @@ def get_base_data(data_name):
         return JPLDataOutputModel(items=results)
     else:
         return JPLDataOutputModel(items=[])
+
+
+@router.get("/anomaly-data", response_model=GeneralAnomalyTotalModel, status_code=200)
+def get_base_data(data_name: str, window_range_by_day: int, sigma_count: int):
+    """
+    This method returns a data of series jbl
+    https://sideshow.jpl.nasa.gov/pub/JPL_GPS_Timeseries/repro2018a/post/point/AB27.series
+    @return: JPLDataOutputModel
+    """
+    result = JPLService(jpl_client=JPLClient).get_anomaly_data(data_name=data_name,
+                                         window_range_by_day=window_range_by_day,
+                                         sigma_count = sigma_count
+                                         )
+    if result is not None:
+        return result
+    else:
+        return GeneralAnomalyTotalModel.to_model(window_range=window_range_by_day,
+                                                 sigma_count=sigma_count,
+                                                 east_items=[],
+                                                 north_items=[],
+                                                 vert_items=[]
+                                                 )
