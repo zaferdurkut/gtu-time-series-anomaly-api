b856cc7328f9e6bf3bb00b634df2571bbe3e9a70
Zafer Durkut <durkutzafer@gmail.com>
2020-05-26 12:36:31 +0300

    JPLService controller  was updated

--- src/api/jpl_controller.py
+++ src/api/jpl_controller.py
@@ -31,6 +31,9 @@ def get_base_data(data_name):
     """
     This method returns a data of series jbl
     https://sideshow.jpl.nasa.gov/pub/JPL_GPS_Timeseries/repro2018a/post/point/AB27.series
+
+    @param data_name: name of the jpl data \t
+
     @return: JPLDataOutputModel
     """
     result = JPLClient.get_data(data_name=data_name)
@@ -46,6 +49,11 @@ def get_base_data(data_name: str, window_range_by_day: int, sigma_count: int):
     """
     This method returns a data of analysis jbl results and a list of anomaly data
     https://sideshow.jpl.nasa.gov/pub/JPL_GPS_Timeseries/repro2018a/post/point/AB27.series
+
+    @param data_name: name of the jpl data \t
+    @param window_range_by_day: time interval to look \t
+    @param sigma_count: tolerance range for standart deviation \t
+
     @return: GeneralAnomalyTotalModel
     """
     result = JPLService(jpl_client=JPLClient).get_anomaly_data(data_name=data_name,
